f892b34feb434bbd4d81b4306b5ed72635e553ab
Pavel <cemenru@mail.ru>
2022-11-29 22:17:56 +0300

    rendering card

--- js/renderCard.js
+++ js/renderCard.js
@@ -1,8 +1,8 @@
 'use strict';
+const cardsContainer = document.querySelector('.top__card');
 
-const renderCards = (cards) => {
- 
-  fetch('/db/price.json')
+
+const pullCard = () => fetch('/db/price.json')
   .then(res => {
     if (res.ok) {
       return res.json()
@@ -13,10 +13,8 @@ const renderCards = (cards) => {
   })
   .then(data => {
     data.forEach(card => {
-
-      const cardsContainer = document.querySelector('.top__card');
       const cardBlock = document.createElement('div');
-      cardBlock.classList.add('card');
+      cardBlock.classList.add('cardTop');
       cardBlock.id = card.id;
       cardBlock.innerHTML = `
           <div class="card__description">
@@ -29,7 +27,7 @@ const renderCards = (cards) => {
               </div>
               <div class="card__description-color">
                 <span class="color-1 color" style="background-color: #${card.catalog.color1};"></span>
-                <span class="color-2 color" style="background-color: #${card.catalog.color1};"></span>
+                <span class="color-2 color" style="background-color: #${card.catalog.color2};"></span>
                 <span class="color-3 color" style="background-color: #${card.catalog.color3};"></span>
                 <span class="color-4 color" style="background-color: #${card.catalog.color4};"></span>
                 <span class="color-5 color" style="background-color: #${card.catalog.color5};"></span>
@@ -41,7 +39,7 @@ const renderCards = (cards) => {
             <div class="card__price">
               <div class="card__price-content">
                 <p class="content__color">${card.catalog.сolorName1}</p>
-                <p class="content__text">500 руб кв/м</p>
+                <p class="content__text"><span>420</span> руб кв/м</p>
               </div>
               <button class="card__price-add">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="19" viewBox="0 0 20 19" fill="none">
@@ -49,20 +47,38 @@ const renderCards = (cards) => {
                 </svg>
               </button>
             </div>
-        `
+        `;
       cardsContainer.append(cardBlock);
     });
-
-  });
-}
-renderCards();
-
-const colors = document.querySelectorAll('.color');
-console.log(colors);
+    const cardsTop = document.querySelectorAll('.cardTop');
+    const contentColor = document.querySelectorAll('.content__color');
+    const contentPrice = document.querySelectorAll('.content__text > span');
 
 
+    cardsTop.forEach((card, cardIdx) => {
+      card.addEventListener('click', (event) => {
+        if (event.target.classList.contains('color')) {
+          const numb = event.target.className.slice(6, 7);
+          const numbColor = 'сolorName' + numb;
+          const numbPrice = 'price' + numb;
+          contentColor.forEach((color, colorIdx) => {
+            if (cardIdx === colorIdx) {
+              color.innerHTML = data[cardIdx].catalog[numbColor];
+            }
+          })
 
-
-
+          contentPrice.forEach((price, priceIdx) => {
+            if (cardIdx === priceIdx) {
+              price.innerHTML = data[cardIdx].catalog[numbPrice]
+            }
+          })
+        }
+      })
+    })
+  })
+  .catch(error => {
+    alert('Ошибка: ' + error.messenger)
+  })
+pullCard();
 
 
